3f565d89a7c9aeee2d8d14ee73ac26596e77f346
jondashkyle <contact@jon-kyle.com>
2017-09-10 02:26:02 -0400

    writing

--- site/views/log.js
+++ site/views/log.js
@@ -1,7 +1,7 @@
 var html = require('choo/html')
 var objectValues = require('object-values')
 var wrapper = require('../components/wrapper')
-var entry = require('../components/entries')
+var entry = require('../components/entries/longform')
 
 module.exports = wrapper(log)
 
@@ -11,9 +11,44 @@ function log (state, emit) {
       ${objectValues(state.page.children)
         .filter(page => !page.draft)
         .reverse()
-        .map(entry)
+        .map(container)
       }
     </div>
   `
+
+  function container (props) {
+    return html`
+      <div>
+        <div class="py1">
+          ${entry(props)}
+        </div>
+        <div class="c12 p1"><div class="bb1"></div></div>
+      </div>
+    `
+  }
 }
 
+
+function thumbnail (props) {
+  var image = props.files[props.thumbnail] || { }
+  if (props.draft) return // skip the drafts
+
+  return html`
+    <div class="p1 c6 mb1" md="c12">
+      <a href="${props.url}" class="db">
+        <div
+          class="psr bggreylight ${props.thumbnailborder ? 'ol1' : ''}"
+          style="padding-bottom: ${image.ratioY * 100}%"
+        >
+          <img src="${image.url}" class="psa t0 l0 c12" />
+        </div>
+        <div class="py1">
+          <h3>${props.title}</h3>
+        </div>
+        <div>
+          ${props.excerpt}
+        </div>
+      </div>
+    </div>
+  `
+}
